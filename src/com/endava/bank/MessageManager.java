d8f01c2b7003a6f3d00452e6447d6521f4eb100f
atorres <alejo.novoa@misena.edu.co>
2019-02-06 09:47:00 -0500

    Observer pattern implemented

--- src/com/endava/bank/MessageManager.java
+++ src/com/endava/bank/MessageManager.java
@@ -5,6 +5,8 @@ public class MessageManager implements TransactionObserver {
 
     @Override
     public void  update(Transaction currentTransaction){
-
+        MktServiceConcret mktservice = new MktServiceConcret();
+        Customer customer = currentTransaction.getRequest().getCustomer();
+        mktservice.updateInformation(customer.getId(), currentTransaction.getRequest().getAmount(),customer.getEmail());;
     };
 }
